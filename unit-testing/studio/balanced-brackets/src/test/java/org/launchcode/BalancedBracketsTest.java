9936cb5ac3f93c0520e6b41fd29fdd692ad949fd
Landon <landon.hedrick05@gmail.com>
2023-09-11 20:11:30 -0500

    Added unit test and changed functionality to account for tests

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,88 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    // Test 1
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    // Test 2
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    // Test 3
+    @Test
+    public void doesNotHaveBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    // Test 4
+    @Test
+    public void hasBalancedBracketsWithStringTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    // Test 5
+    @Test
+    public void hasBalancedBracketsWithNumbersTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[12345]"));
+    }
+
+    // Test 6
+    @Test
+    public void hasBalancedBracketsWithStringFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launchcode"));
+    }
+
+    // Test 7
+    @Test
+    public void hasBalancedBracketsWithNumbersFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[12345"));
+    }
+
+    // Test 8
+    @Test
+    public void hasNoBracketsWithString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("TestString"));
+    }
+
+    // Test 9
+    @Test
+    public void hasNoBracketsWithNum() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("12345"));
+    }
+
+    // Test 10
+    @Test
+    public void hasWrongFacingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    // Test 11
+    @Test
+    public void hasBalancedBracketsWithStringAndOppFacingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    // Test 12
+    @Test
+    public void hasBalancedBracketsNotSurroundingString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    // Test 13
+    @Test
+    public void hasBalancedBracketsWithStringAndBracketsInMiddle() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[]Code"));
+    }
+
+    // Test 14
+    @Test
+    public void doesntHave() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[]][Code"));
+    }
 }
